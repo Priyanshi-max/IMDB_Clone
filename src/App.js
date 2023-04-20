31ecdcc2518214bee9f4b2471ae15071a918a29e
Priyanshi-max <67965834+Priyanshi-max@users.noreply.github.com>
2023-04-20 15:40:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import MovieList from './components/movieList/movieList';
+import Movie from './pages/MovieDetail/Movie';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Header />
+        <Routes>
+           <Route index element ={<Home/>}></Route>
+           <Route path='movie/:id' element={<Movie/>}></Route>
+           <Route path='movies/:type' element={<MovieList/>}></Route>
+           <Route path='/*' element={<h1>Error Page</h1>}></Route>
+        </Routes>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
